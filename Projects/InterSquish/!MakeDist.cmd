e9b5c35e50791d061bc456cf5086215afaea7b59
SlimShaggy <slimshaggy@live.com>
2018-09-24 00:00:00 +0300

    Created release 2.0.5.21

--- Projects/InterSquish/!MakeDist.cmd
+++ Projects/InterSquish/!MakeDist.cmd
@@ -1,4 +1,9 @@
+@echo off
 rem SetCRC\SetCRC IS.exe suvi
 rem zip iss1704 -r -9 IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
 rem rar a iss -m5 -s IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
-7z a -mx=9 iss.zip IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
+setlocal
+set filename=iss.zip
+if exist %filename% (del %filename%)
+7z a -mx=9 %filename% IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
+endlocal
