38c2777b6c23afa32c829d8aa6c20cec61ec297e
SlimShaggy <slimshaggy@live.com>
2018-09-25 22:54:33 +0300

    Created release 2.0.5.20

--- Projects/InterSquish/!MakeDist.cmd
+++ Projects/InterSquish/!MakeDist.cmd
@@ -1,3 +1,4 @@
 rem SetCRC\SetCRC IS.exe suvi
 rem zip iss1704 -r -9 IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
-rar a iss -m5 -s IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
+rem rar a iss -m5 -s IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
+7z a -mx=9 iss.zip IS*.exe *.diz *.txt *.cfg SCRIPTS\*.* DOC\RUS\*.* DOC\ENG\*.*
